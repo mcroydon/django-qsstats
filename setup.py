5664a7391f1cb430fa341c77cb95078b4035725c
Matt Croydon <matt@lawrence.com>
2010-03-29 01:40:22 -0500

    Somewhat redundant.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='django-qsstats',
     version='0.2.0',
-    description='A django microframework that eases the generation of aggregate data for querysets over time.',
+    description='A django microframework that eases the generation of aggregate data for querysets.',
     author='Matt Croydon',
     author_email='mcroydon@gmail.com',
     url='http://github.com/mcroydon/django-qsstats/',
